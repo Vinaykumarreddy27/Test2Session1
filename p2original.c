194967cc0db49728374a8f3b747ed32e255af900
k.Vinay kumar Reddy <94587844+Vinaykumarreddy27@users.noreply.github.com>
2022-02-24 04:03:49 +0000

    checking prime number

---
+++ p2original.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+voidinput(float*x1,float*y1,float*x2,float*y2,float*x3,float*y3);
+{
+  printf("enter vertex one");
+  scanf("%f%f",x1,y1);
+  printf("enter vertex two");
+  scanf("%f%f",x2,y2);
+  printf("enter vertex three");
+  scanf("%f%f",x3,y3);
+  }
+int 
+    
+}
